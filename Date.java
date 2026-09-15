b022326ff739cb70b8e0e93de8f172df974e1804
trumpercharles <trumper.charles@student.ccm.edu>
2026-09-15 14:43:02 -0400

    Started Date

--- Date.java
+++ Date.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Date {
-    
+    public static void main(String args[]){
+        String[] months={"January","February","March","April","May","June","July","August","September","October","November","December"};
+        int month=0, day=0, year=0;
+
+        Scanner newScanner=new Scanner(System.in);
+        System.out.print("Enter the day: ");
+        day=newScanner.nextInt();
+
+        System.out.print("Enter the month: ");
+        month=newScanner.nextInt();
+
+        System.out.print("Enter the year: ");
+        year=newScanner.nextInt();
+
+        System.out.print(month+"/"+day+"/"+year + "\n"+months[month-1]+" "+day+", "+year + "\n"+day+" "+months[month-1]+" "+year);
+    }
 }
