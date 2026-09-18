4048261c65e1912e476c033ec7a3fb5012c2fb64
trumpercharles <trumper.charles@student.ccm.edu>
2026-09-18 13:22:07 -0400

    Finished Employee

--- Employee.java
+++ Employee.java
@@ -1,3 +1,58 @@
 public class Employee {
-    
+    public String name,department,position;
+    public int idNumber;
+
+    public Employee(String Ename, int EidNumber, String Edepartment, String Eposition){
+        name=Ename;
+        idNumber=EidNumber;
+        department=Edepartment;
+        position=Eposition;
+    }
+
+    public Employee(String Ename, int EidNumber){
+        name=Ename;
+        idNumber=EidNumber;
+        department="";
+        position="";
+    }
+
+    public Employee(){
+        name="";
+        idNumber=0;
+        department="";
+        position="";
+    }
+
+    public void ChangeName(String Nname){
+        name=Nname;
+    }
+
+    public void ChangeID(int ID){
+        idNumber=ID;
+    }
+
+    public void ChangeDepartment(String Ndepartment){
+        department=Ndepartment;
+    }
+
+    public void ChangePosition(String NPosition){
+        position=NPosition;
+    }
+
+    public String GetName(){
+        return name;
+    }
+
+    public int getID(){
+        return idNumber;
+    }
+
+    public String GetDepartment(){
+        return department;
+    }
+
+    public String GetPosition(){
+        return position;
+    }
+
 }
