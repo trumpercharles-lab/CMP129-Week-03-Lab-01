a3e57d46bb45e364909d05ea0a87d363847b676d
trumpercharles <trumper.charles@student.ccm.edu>
2026-09-18 13:11:33 -0400

    Finished DateTest

--- Date.java
+++ Date.java
@@ -1,20 +1,9 @@
-import java.util.Scanner;
-
 public class Date {
-    public static void main(String args[]){
+
+    public void DisplayDate(int day, int month, int year){
         String[] months={"January","February","March","April","May","June","July","August","September","October","November","December"};
-        int month=0, day=0, year=0;
-
-        Scanner newScanner=new Scanner(System.in);
-        System.out.print("Enter the day: ");
-        day=newScanner.nextInt();
-
-        System.out.print("Enter the month: ");
-        month=newScanner.nextInt();
-
-        System.out.print("Enter the year: ");
-        year=newScanner.nextInt();
 
         System.out.print("\n"+month+"/"+day+"/"+year + "\n"+months[month-1]+" "+day+", "+year + "\n"+day+" "+months[month-1]+" "+year);
     }
+
 }
--- DateTest.java
+++ DateTest.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class DateTest {
+    
+    public static void main(String args[]){
+        int day=0, month=0, year=0;
+
+        Scanner newScanner=new Scanner(System.in);
+        System.out.print("Enter the day: ");
+        day=newScanner.nextInt();
+
+        System.out.print("Enter the month: ");
+        month=newScanner.nextInt();
+
+        System.out.print("Enter the year: ");
+        year=newScanner.nextInt();
+
+        Date DateObject=new Date();
+        DateObject.DisplayDate(day, month, year);
+    }
     
 }
